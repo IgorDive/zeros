a27234e39bd92b23743a118e096f9a976aa28546
IgorDive <ihar.kazlovich@gmail.com>
2018-02-28 12:09:42 +0300

    first modified src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let counterZero = 0;
+  let i = 1;
+
+  while ( number / Math.pow(5, i) >= 1 ) {
+  counterZero += Math.floor( number / Math.pow(5, i) );
+  i += 1;
+  }
+
+  return counterZero;
 }
